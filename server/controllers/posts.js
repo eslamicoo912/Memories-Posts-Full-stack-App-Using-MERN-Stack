e408b2e1ccbdb5b07316cf4422cc853c94063acd
eslamicoo912 <eslamicoo3@gmail.com>
2022-09-10 23:44:34 +0200

    add controllers functions

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -1,3 +1,22 @@
-export const getPosts = (req, res) => {
-  res.send("POSTS");
+import PostMessage from "../models/postMessage.js";
+
+export const createPost = async (req, res) => {
+  const post = req.body;
+  const newPost = new PostMessage(post);
+
+  try {
+    await newPost.save();
+    res.status(200).json(newPost);
+  } catch (error) {
+    console.log(error.message);
+  }
+};
+
+export const getPosts = async (req, res) => {
+  try {
+    const posts = await PostMessage.find();
+    res.status(200).json(posts);
+  } catch (error) {
+    console.log(error.message);
+  }
 };
--- server/routes/posts.js
+++ server/routes/posts.js
@@ -4,5 +4,6 @@ import * as controllers from "../controllers/posts.js";
 const routes = express.Router();
 
 routes.get("/", controllers.getPosts);
+routes.post("/", controllers.createPost);
 
 export default routes;
