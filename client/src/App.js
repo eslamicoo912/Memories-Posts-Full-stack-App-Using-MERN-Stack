eb2400e30454cbeb093d6cf75448ef3aba45f416
eslamicoo912 <eslamicoo3@gmail.com>
2022-09-10 23:59:01 +0200

    set up the frontend structure and basic files

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,26 @@
 import React from "react";
+import memo from "./images/memo.jpg";
+import Posts from "./components/Posts/Posts";
+import Form from "./components/Form/Form";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div className="app container mt-5 text-center">
+      <header>
+        <h1>Memories</h1>
+        <img src={memo} className="w-50" alt="logo" />
+      </header>
+      <div className="content row">
+        <div className="col-5">
+          <Posts />
+        </div>
+        <div className="col"></div>
+        <div className="col-5">
+          <Form />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default App;
