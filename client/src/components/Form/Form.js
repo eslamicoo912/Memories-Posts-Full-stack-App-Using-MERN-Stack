bf46164d291fdd4e82402b7563f20f09f074d457
eslamicoo912 <eslamicoo3@gmail.com>
2022-09-11 14:37:10 +0200

    creating the form

--- client/src/components/Form/Form.js
+++ client/src/components/Form/Form.js
@@ -1,7 +1,57 @@
-import React from "react";
+import React, { useState } from "react";
+import "./Form.css";
 
 const Form = () => {
-  return <h1>Form</h1>;
+  const [postData, setPostData] = useState({
+    creator: "",
+    title: "",
+    message: "",
+    tags: "",
+  });
+
+  const handleSubmit = () => {};
+  const clear = () => {};
+  return (
+    <>
+      <form onSubmit={handleSubmit}>
+        <h4>Creating a memory</h4>
+        <input
+          name="creator"
+          value={postData.creator}
+          onChange={(e) =>
+            setPostData({ ...postData, creator: e.target.value })
+          }
+        />
+        <input
+          name="title"
+          value={postData.title}
+          onChange={(e) => setPostData({ ...postData, title: e.target.value })}
+        />
+        <input
+          name="message"
+          value={postData.message}
+          onChange={(e) =>
+            setPostData({ ...postMessage, message: e.target.value })
+          }
+        />
+        <input
+          name="tags"
+          value={postData.tags}
+          onChange={(e) => setPostData({ ...postData, tags: e.target.value })}
+        />
+        <button
+          className="btn btn-primary"
+          type="submit"
+          onClick={handleSubmit}
+        >
+          Submit
+        </button>
+        <button className="btn btn-danger" onClick={clear}>
+          Clear
+        </button>
+      </form>
+    </>
+  );
 };
 
 export default Form;
