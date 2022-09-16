b259998bc07cc761b5574d43fa084767fcd3b2dd
eslamicoo912 <eslamicoo3@gmail.com>
2022-09-17 00:31:56 +0200

    create the form of register page

--- client/src/components/Register/Register.js
+++ client/src/components/Register/Register.js
@@ -1,7 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Register = () => {
-  return <div>Register</div>;
+  const [formData, setFormData] = useState({
+    username: "",
+    password: "",
+  });
+
+  const handleChange = () => {};
+
+  const handleSubmit = () => {};
+
+  return (
+    <div className="text-center mt-5 pt-5">
+      <h2>Create New Account</h2>
+      <form onSubmit={handleSubmit} className="mt-4">
+        <input
+          className="form-control w-25 my-4 m-auto"
+          placeholder="username"
+          type="text"
+          name="username"
+          value={formData.username}
+          onChange={handleChange}
+        />
+        <input
+          className="form-control w-25 my-4 m-auto"
+          placeholder="password"
+          type="password"
+          name="password"
+          value={formData.username}
+          onChange={handleChange}
+        />
+        <button className="btn btn-dark" type="submit">
+          Submit
+        </button>
+      </form>
+    </div>
+  );
 };
 
 export default Register;
